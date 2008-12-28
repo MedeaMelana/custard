9102035872112138a04bfff07974e386c0d1dce5
MedeaMelana <MedeaMelana@2f27edba-c156-11dd-ba5c-d3b7c2b3b534>
2008-12-28 21:32:21 +0000

    Added wardrobe room.

--- World.hs
+++ World.hs
@@ -12,8 +12,10 @@ mkWorld = do
   living <- mkRoom "A warm living room"
   kitchen <- mkRoom "A clean kitchen"
   bedroom <- mkRoom "A cozy bedroom"
+  wardrobe <- mkRoom "Inside a pitch dark wardrobe"
 
   mkExits garden [(north, living)]
   mkExits living [(south, garden), (east, kitchen), (up, bedroom)]
   mkExits kitchen [(west, living)]
-  mkExits bedroom [(down, living)]
+  mkExits bedroom [(down, living), ("wardrobe", wardrobe)]
+  mkExits wardrobe [("out", bedroom)]
