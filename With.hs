c858346e77d30890e5e5ca2d82c342fd63321f71
MedeaMelana <MedeaMelana@2f27edba-c156-11dd-ba5c-d3b7c2b3b534>
2013-09-01 16:15:03 +0000

    Update with to work with newer versions of data-accessor

--- With.hs
+++ With.hs
@@ -1,16 +1,12 @@
-module With (withBy, with) where
+module With (with) where
 
 import Data.Accessor
 -- import Data.Accessor.Tuple
 import Control.Monad.State
 
--- | Causes all state actions in the argument to be computed relative to the accessor.
-withBy :: MonadState a m => (n b c -> b -> b) -> Accessor a b -> n b c -> m ()
-withBy run field action = field %: run action
-
 -- | 'withBy' specialized for 'State'.
-with :: MonadState a m => Accessor a b -> State b () -> m ()
-with = withBy execState
+with :: Monad m => Accessor a b -> State b () -> StateT a m ()
+with field action = field %: execState action
 
 -- example :: State (Int, (Bool, Char)) ()
 -- example = do
